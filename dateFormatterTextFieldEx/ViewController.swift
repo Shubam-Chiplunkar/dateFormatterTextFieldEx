76726d7a4edb58dba312ea808b195efde8474613
Shubham Chiplunkar <shubhamchiplunkar21@gmail.com>
2023-03-31 17:53:13 +0530

    date formatter

--- dateFormatterTextFieldEx/ViewController.swift
+++ dateFormatterTextFieldEx/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var date3TextField: UITextField!
+    @IBOutlet weak var date2TextField: UITextField!
+    @IBOutlet weak var date4TextField: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func date(_ sender: Any) {
+        
+        let now = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MM/dd/yyyy, hh:mm:ss a"
+        dateTextField.text = dateFormatter.string(from: now)
+        
+        dateFormatter.dateStyle = .full
+        date2TextField.text = dateFormatter.string(from: now)
+        
+        dateFormatter.dateFormat = "EE, MM/dd/yy , hh:mm a"
+        date3TextField.text = dateFormatter.string(from: now)
+        
+        dateFormatter.dateFormat = "MM/dd/yy, hh:mm:ss"
+        date4TextField.text = dateFormatter.string(from: now)
+        
+    }
 
 }
 
